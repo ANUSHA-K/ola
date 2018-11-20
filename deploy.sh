c850a50d7ff58827a025d4c3a62141f762ba7c39
ANUSHA-K <anushak415@gmail.com>
2018-11-20 15:56:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $ENVIRNOMENT = "QA" ]; then
   do
    echo "deploy war file into $i "
   sleep 3
-  sshpass -p "1234" scp target/ola.war anusha@$i:/home/anusha/softwares/apache-tomcat-8.5.34/webapps
+  sshpass -p "1234" scp target/*.war anusha@$i:/home/anusha/softwares/apache-tomcat-8.5.34/webapps
   
   echo " starting tomcat server $i "
   sshpass -p "1234" ssh anusha@$i "JAVA_HOME=/home/anusha/softwares/jdk1.8.0_181 " "/home/anusha/softwares/apache-tomcat-8.5.34/bin/startup.sh"
